5e1dc751db91af481d60a5858949464950d7ce1d
真 罗 <1959132915@qq.com>
2020-12-07 18:16:12 +0800

    添加项目文件。

---
+++ TEST/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TEST
+{
+    class Program
+    {
+        static int Main(String[] args)
+        {
+            Console.WriteLine("hello world!");
+            foreach (var s in args)
+            {
+                Console.WriteLine(s);
+            }
+
+            return 0;
+        }
+    }
+}
